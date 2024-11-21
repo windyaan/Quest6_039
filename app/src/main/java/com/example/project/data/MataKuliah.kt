6a95fb1237517757e3f1de9feff0c8a0ecad91ab
windyaan <windyanurmaretha@gmail.com>
2024-11-22 05:46:26 +0700

    daftar piliihan MK

--- app/src/main/java/com/example/project/data/MataKuliah.kt
+++ app/src/main/java/com/example/project/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.project.data
 
 object MataKuliah {
+    val option= listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
