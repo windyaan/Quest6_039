778d7752b3a295ba5667309b84aaf3f8a94cecf1
windyaan <windyanurmaretha@gmail.com>
2024-11-22 06:02:34 +0700

    tambah icon

--- app/src/main/java/com/example/project/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project/ui/view/screen/RencanaStudyView.kt
@@ -2,6 +2,7 @@ package com.example.project.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -73,6 +77,13 @@ fun RencanaStudyView(
                     fontWeight = FontWeight.Light,
                     fontSize = 12.sp,
                     color = Color.White
+                )
+            }
+            Box(){
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
                 )
             }
         }
