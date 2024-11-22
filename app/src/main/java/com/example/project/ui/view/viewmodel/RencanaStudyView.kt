0e943f90a3130dbd6dff678b7ec01f63a61d5c40
windyaan <windyanurmaretha@gmail.com>
2024-11-22 07:10:40 +0700

    variabel nilai tidak dapat diubah

--- app/src/main/java/com/example/project/ui/view/viewmodel/RencanaStudyView.kt
+++
@@ -1,4 +0,0 @@
-package com.example.project.ui.view.viewmodel
-
-class RencanaStudyView{
-}
---
+++ app/src/main/java/com/example/project/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.project.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.project.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RencanaStudyViewModel : ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+}
