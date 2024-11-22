0ee6026c3e19b3071e3875e95b935acab14a7425
windyaan <windyanurmaretha@gmail.com>
2024-11-22 07:14:01 +0700

    fungsi MK

--- app/src/main/java/com/example/project/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/project/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,8 +5,12 @@ import com.example.project.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel(){
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+    fun setMatakuliah (mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy ( namaMK = mkPilihan) }
+    }
 }
